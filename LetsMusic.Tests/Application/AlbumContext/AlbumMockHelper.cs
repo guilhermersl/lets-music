2a7b3f9d9d3c0d7cd9258f678d8675fbeebb57e3
Guilherme Rodrigues dos Santos Leitao <guilherme.santos@bancoarbi.com.br>
2022-08-05 09:58:59 -0300

    Teste para verificar subida com sucesso

--- LetsMusic.Tests/Application/AlbumContext/AlbumMockHelper.cs
+++ LetsMusic.Tests/Application/AlbumContext/AlbumMockHelper.cs
@@ -14,6 +14,7 @@ namespace LetsMusic.Tests.Application.AlbumContext
         {
             return new List<MusicaOutputDto>
                    {
+                        //Mocks
                         MockMusicaOutputDto(),
                         MockMusicaOutputDto(),
                         MockMusicaOutputDto()
